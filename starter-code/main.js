af0d5670159aaad7206bd28d07a95fd5db9ae435
Mario Maia <mamsm77@gmail.com>
2016-10-22 13:51:20 +1100

    added JS function for cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -11,3 +11,25 @@ if (CardTwo === CardFour) {
 		alert ('Sorry, try again')
 }
 		
+
+// find the board and set it to a variable
+// so that we can add cards to it
+var board = document.getElementById('game-board');
+
+// function that will create your board
+function createCards() {
+  // loop through your cards array to create HTML card elements for your board
+  for (var i=0; i<cards.length; i++) {
+
+    // create a div element which will be used as a card
+    var cardElement = document.createElement('div');
+
+    // add a class to the card element which will help link styling on CSS
+    cardElement.className = 'card';
+
+    // append the card to the board
+    board.appendChild(cardElement);
+  }
+
+}
+
